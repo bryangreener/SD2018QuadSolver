290f95196ade1a60541567ce7e5902105059669d
Bryan Greener <Bryan.c.greener@wmich.edu>
2018-02-21 16:59:19 +0000

    Start making unit test with Check. Need to fix some errors.

--- spikes/v0-1/quad.c
+++ spikes/v0-1/quad.c
@@ -3,18 +3,29 @@
 #include<string.h>
 #include<math.h>
 
-void QuadSolve(double a, double b, double c);
+typedef struct Tuple{
+  double r1;
+  double r2;
+} Tuple;
+
+
+
+
+
+Tuple QuadSolve(double a, double b, double c);
 int main(int argc, char *argv[])
 {
-  QuadSolve(atof(argv[1]), atof(argv[2]), atof(argv[3]));
+  Tuple result = QuadSolve(atof(argv[1]), atof(argv[2]), atof(argv[3]));
   return 0;
 }
 
-void QuadSolve(double a, double b, double c){
+Tuple QuadSolve(double a, double b, double c){
   double d = (b*b) - (double)(4.0 * (a*c));
   double s = sqrt(d);
   double r1 = (-b + s) / (double)(2.0*a);
   double r2 = (-b - s) / (double)(2.0*a);
   printf("%.7e\t%.7e\n", r1, r2);
+  Tuple result = { r1, r2 };
+  return result;
 } 
 
